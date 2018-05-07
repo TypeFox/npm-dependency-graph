4ed91e03ae7d8f6cb5fdf57c4347aa3082af2a59
Miro Spönemann <miro.spoenemann@typefox.io>
2018-05-07 14:02:19 +0200

    Renamed package to 'depgraph-navigator'

--- standalone-app/src/frontend/main.ts
+++ standalone-app/src/frontend/main.ts
@@ -10,7 +10,7 @@
 import 'reflect-metadata'
 import * as jQuery from 'jquery';
 import { TYPES } from 'sprotty/lib';
-import { containerFactory, DepGraphModelSource } from 'dependency-graph/lib/browser';
+import { containerFactory, DepGraphModelSource } from 'depgraph-navigator/lib/browser';
 
 const container = containerFactory();
 const modelSource = container.get<DepGraphModelSource>(TYPES.ModelSource);
